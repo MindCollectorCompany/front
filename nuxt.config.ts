2fd6789679690f6cb84f3041cfe8b4dc12004985
LAPTOP-FI0GUN78\mau10 <amonr51@gmail.com>
2023-06-03 10:01:07 +0300

    remove vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import vuetify from "vite-plugin-vuetify";
-import { createResolver } from "@nuxt/kit";
-const { resolve } = createResolver(import.meta.url);
-
 export default defineNuxtConfig({
   css: [
     "~/assets/css/main.css",
-    "vuetify/lib/styles/main.sass",
-    "@mdi/font/css/materialdesignicons.min.css",
   ],
-  build: {
-    transpile: ["vuetify"],
-  },
   vite: {
     define: {
       "process.env.DEBUG": false,
@@ -29,13 +20,4 @@ export default defineNuxtConfig({
       pathPrefix: false,
     },
   ],
-  hooks: {
-    "vite:extendConfig": (config) => {
-      config.plugins?.push(
-        vuetify({
-          styles: { configFile: resolve("./settings.scss") },
-        })
-      );
-    },
-  },
 });
