3a6d225b38b15ec3cf9fdef9d1db47ac79126be8
LAPTOP-FI0GUN78\mau10 <amonr51@gmail.com>
2023-06-03 11:04:38 +0300

    add naive-ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: [
-    "~/assets/css/main.css",
-  ],
+  css: ["~/assets/css/main.css"],
   vite: {
     define: {
       "process.env.DEBUG": false,
+    },
+    optimizeDeps: {
+      include:
+        process.env.NODE_ENV === "development"
+          ? ["naive-ui", "vueuc", "date-fns-tz/esm/formatInTimeZone"]
+          : [],
     },
   },
   postcss: {
@@ -20,4 +24,15 @@ export default defineNuxtConfig({
       pathPrefix: false,
     },
   ],
+  build: {
+    transpile:
+      process.env.NODE_ENV === "production"
+        ? [
+            "naive-ui",
+            "vueuc",
+            "@css-render/vue3-ssr",
+            "@juggle/resize-observer",
+          ]
+        : ["@juggle/resize-observer"],
+  },
 });
