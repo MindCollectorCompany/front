937b356c622bb4e998a07c166c9c38fc31886504
LAPTOP-FI0GUN78\mau10 <amonr51@gmail.com>
2023-06-03 18:53:36 +0300

    add components*
    common/Dropdown (prototype)
    nav/ProjectsList
    nav/Settings

---
+++ components/common/Dropdown.vue
@@ -0,0 +1,43 @@
+<template>
+  <NDropdown
+    trigger="hover"
+    placement="bottom-start"
+    :options="options"
+    @select="handleSelect"
+  >
+    <NButton>Projects</NButton>
+  </NDropdown>
+</template>
+
+<script lang="ts" setup>
+import { NDropdown, NButton } from "naive-ui";
+</script>
+
+<!-- TODO add props support -->
+<script lang="ts">
+export default {
+  data() {
+    return {
+      options: [
+        {
+          label: "Project 1",
+          key: "p-1",
+        },
+        {
+          label: "Project 2",
+          key: "p-2",
+        },
+        {
+          label: "Add new project",
+          key: "new",
+        },
+      ],
+    };
+  },
+  methods: {
+    handleSelect(key: string | number) {
+      navigateTo(`/projects/${key}`);
+    },
+  },
+};
+</script>
---
+++ components/nav/Settings.vue
@@ -0,0 +1,64 @@
+<template>
+  <NDropdown
+    trigger="hover"
+    placement="bottom-start"
+    :options="options"
+    @select="handleSelect"
+  >
+    <NButton>Settings</NButton>
+  </NDropdown>
+</template>
+
+<script lang="ts" setup>
+import { NDropdown, NButton } from "naive-ui";
+</script>
+
+<script lang="ts">
+enum KeyKind {
+  PROJECT_SETTINGS = "project-settings",
+  ACCOUNT = "account",
+  LOG_OUT = "log-out",
+}
+
+export default {
+  data() {
+    return {
+      options: [
+        {
+          label: "Project settings",
+          key: KeyKind.PROJECT_SETTINGS,
+        },
+        {
+          label: "Account",
+          key: KeyKind.ACCOUNT,
+        },
+        {
+          label: "Log out",
+          key: KeyKind.LOG_OUT,
+        },
+      ],
+    };
+  },
+  methods: {
+    handleSelect(key: string | number) {
+      switch (key) {
+        case KeyKind.PROJECT_SETTINGS: {
+          navigateTo(`/project-settings`);
+          break;
+        }
+        case KeyKind.ACCOUNT: {
+          navigateTo(`/auth/account`);
+          break;
+        }
+        case KeyKind.LOG_OUT: {
+          navigateTo(`/auth/log-out`);
+          break;
+        }
+        default: {
+          navigateTo(`/`);
+        }
+      }
+    },
+  },
+};
+</script>
