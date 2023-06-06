3e213f70bd0c91c7c2725b305e1a30977739c2df
LAPTOP-FI0GUN78\mau10 <amonr51@gmail.com>
2023-06-06 12:44:43 +0300

    add PageBlock, place on projects page

---
+++ components/PageBlock.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col gap-4 border-2">
+    <div class="flex items-center justify-between">
+      <span>{{ title }}</span>
+      <span>
+        <Button @click="btnClick" type="info">{{ buttonText }}</Button>
+      </span>
+    </div>
+    <div><slot></slot></div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: ["title", "buttonText", "btnClick"],
+};
+</script>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,3 +1,13 @@
 <template>
-  <div>All projects</div>
+  <div class="flex flex-col gap-4">
+    <PageBlock title="Page title" buttonText="main btn">
+      some content 1
+    </PageBlock>
+    <PageBlock title="Sub title" buttonText="main btn"
+      >Some content 2</PageBlock
+    >
+    <PageBlock title="Sub title" buttonText="main btn"
+      >Some content 3</PageBlock
+    >
+  </div>
 </template>
