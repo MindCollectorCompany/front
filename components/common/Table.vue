7fd3e9376e955689e05ac1f46fd0382edefd2f78
LAPTOP-FI0GUN78\mau10 <amonr51@gmail.com>
2023-06-08 19:19:59 +0300

    add Table component

---
+++ components/common/Table.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <NTable :bordered="false" :single-line="false">
+      <thead>
+        <tr>
+          <th v-for="col in cols">
+            {{ col }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="row in rows">
+          <td>
+            {{ row.id }}
+          </td>
+          <td>
+            <NavItem
+              :page="`/researches/${row.id}`"
+              :text="row.name"
+              class="hover:text-blue-600"
+            />
+          </td>
+          <td>
+            <span class="inline-block min-w-[100px] font-semibold">
+              ${{ row.balance }}
+            </span>
+            <Button type="primary">Recharge</Button>
+          </td>
+          <td>
+            <Button type="info" @click="$router.push(`/projects/${row.id}`)">
+              Edit
+            </Button>
+          </td>
+        </tr>
+      </tbody>
+    </NTable>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { NTable } from "naive-ui";
+</script>
+
+<script lang="ts">
+export default {
+  props: {
+    cols: {
+      type: Array<String>,
+      required: false,
+    },
+    rows: {
+      type: Array<{
+        id: Number;
+        name: String;
+        balance: Number;
+      }>,
+      required: false,
+    },
+  },
+};
+</script>
