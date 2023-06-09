a44fac281b4018d661ee2e7cdacaf82e4a476111
LAPTOP-FI0GUN78\mau10 <amonr51@gmail.com>
2023-06-09 21:00:00 +0300

    add notification wrapper

--- app.vue
+++ app.vue
@@ -1,15 +1,17 @@
 <template>
   <NMessageProvider>
-    <div class="h-screen bg-[#E5E5E5]">
-      <NuxtLoadingIndicator />
-      <Header />
-      <PageWrapper>
-        <NuxtPage />
-      </PageWrapper>
-    </div>
+    <NNotificationProvider>
+      <div class="h-screen bg-[#E5E5E5]">
+        <NuxtLoadingIndicator />
+        <Header />
+        <PageWrapper>
+          <NuxtPage />
+        </PageWrapper>
+      </div>
+    </NNotificationProvider>
   </NMessageProvider>
 </template>
 
 <script lang="ts" setup>
-import { NMessageProvider } from "naive-ui";
+import { NMessageProvider, NNotificationProvider } from "naive-ui";
 </script>
