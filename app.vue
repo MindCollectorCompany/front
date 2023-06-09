581fccf5727990e8b9ecc2357049558b8d87a640
LAPTOP-FI0GUN78\mau10 <amonr51@gmail.com>
2023-06-09 20:26:55 +0300

    add research page, naive-ui message

--- app.vue
+++ app.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="h-screen bg-[#E5E5E5]">
-    <NuxtLoadingIndicator />
-    <Header />
-    <PageWrapper>
-      <NuxtPage />
-    </PageWrapper>
-  </div>
+  <NMessageProvider>
+    <div class="h-screen bg-[#E5E5E5]">
+      <NuxtLoadingIndicator />
+      <Header />
+      <PageWrapper>
+        <NuxtPage />
+      </PageWrapper>
+    </div>
+  </NMessageProvider>
 </template>
+
+<script lang="ts" setup>
+import { NMessageProvider } from "naive-ui";
+</script>
--- pages/researches/[id].vue
+++ pages/researches/[id].vue
@@ -1,3 +1,38 @@
 <template>
-  <div>Research {{ $route.params.id }}</div>
+  <div>
+    <div>
+      <span class="text-lg">Research {{ $route.params.id }}</span>
+    </div>
+    <div class="mt-8 flex justify-center">
+      <div class="grid max-w-sm grid-cols-2 gap-4">
+        <span>Research name</span>
+        <Input name="name" v-model:text="name" />
+        <span>Research description</span>
+        <Input name="description" v-model:text="description" type="textarea" />
+        <Button type="primary" @click="onSuccess"> Create </Button>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script lang="ts" setup>
+import { useMessage } from "naive-ui";
+
+const message = useMessage();
+
+function onSuccess() {
+  message.success("Research created successfully");
+}
+</script>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      name: "",
+      description: "",
+    };
+  },
+  methods: {},
+};
+</script>
