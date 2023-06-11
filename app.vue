e74646d2ece741ead61542b833ad59d6749eb223
LAPTOP-FI0GUN78\mau10 <amonr51@gmail.com>
2023-06-11 10:19:42 +0300

    add nuxt error boundary

--- app.vue
+++ app.vue
@@ -1,15 +1,21 @@
 <template>
-  <NMessageProvider>
-    <NNotificationProvider>
-      <div class="h-screen bg-[#E5E5E5]">
-        <NuxtLoadingIndicator />
-        <Header />
-        <PageWrapper>
-          <NuxtPage />
-        </PageWrapper>
-      </div>
-    </NNotificationProvider>
-  </NMessageProvider>
+  <NuxtErrorBoundary>
+    <NMessageProvider>
+      <NNotificationProvider>
+        <div class="h-screen bg-[#E5E5E5]">
+          <NuxtLoadingIndicator />
+          <Header />
+          <PageWrapper>
+            <NuxtPage />
+          </PageWrapper>
+        </div>
+      </NNotificationProvider>
+    </NMessageProvider>
+    <!--  -->
+    <template #error="{ error }">
+      <p>An error occurred: {{ error }}</p>
+    </template>
+  </NuxtErrorBoundary>
 </template>
 
 <script lang="ts" setup>
